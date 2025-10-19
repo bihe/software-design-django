5443ab4b623c58f27b5fec3e8ab129cc627b42c0
Henrik Binggl <henrik.binggl@gmail.com>
2025-10-19 15:43:41 +0200

    feat(orders): put the manger into models.py

--- orders/models.py
+++ orders/models.py
@@ -1,8 +1,23 @@
 from django.db import models
 
 from customers.models import Customer
-from orders.managers import OrderManager
 from products.models import Product
+
+"""
+The OrderManager class is a custom manager for the Order model.
+It provides a method to retrieve an order by its ID.
+"""
+
+
+class OrderManager(models.Manager):
+    # This method retrieves an order by its ID
+    def get_by_id(self, id: int) -> models.QuerySet:
+        # First, we get the queryset for all orders with the given ID
+        qs: models.QuerySet = self.get_queryset().filter(id=id)
+        # If there is exactly one order with the given ID, return it
+        if qs.count() == 1:
+            return qs.first()
+        return None
 
 
 # This file defines the models for our application
