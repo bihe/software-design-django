971800fb36bbf5d89452012edc12fa3ffdb56304
Henrik Binggl <henrik.binggl@gmail.com>
2026-01-06 12:57:40 +0100

    feat: use the compose file with a locql sqlite db

--- swd_django_demo/settings_prod.py
+++ swd_django_demo/settings_prod.py
@@ -108,10 +108,10 @@ DATABASES = {
 }
 
 # DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
+#    "default": {
+#        "ENGINE": "django.db.backends.sqlite3",
+#        "NAME": "/opt/django-app/db/db.sqlite3",
+#    }
 # }
 
 LOGGING = {
