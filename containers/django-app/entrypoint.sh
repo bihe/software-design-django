a29c3df36267eec830e1cf1eb320601e5d5cd68f
Henrik Binggl <henrik.binggl@gmail.com>
2026-01-10 15:12:25 +0100

    chore: some comments for containers

--- containers/django-app/entrypoint.sh
+++ containers/django-app/entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 ## initial database migration
-## in a production environment this would typicalle not be put here. the database would be maintained seperately 
+## in a production environment this would typicalle not be put here. the database would be maintained seperately
 python /opt/django-app/manage.py migrate
 python /opt/django-app/manage.py createsuperuser --noinput --username admin --email admin@localhost
-
+# start the django-app with gunicorn
 python /opt/django-app/run_gunicorn.py -w 4 -b 0.0.0.0:8000 swd_django_demo.wsgi:application
+
--- containers/simple/entrypoint.sh
+++ containers/simple/entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 python /opt/django-app/manage.py migrate
 python /opt/django-app/manage.py createsuperuser --noinput --username admin --email admin@localhost
+# star the django development server
+# NOTE: this is not a best-practice approach
 python /opt/django-app/manage.py runserver 0.0.0.0:8000
+
